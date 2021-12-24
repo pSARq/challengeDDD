15bf4c5ff5a07ecfd98549aeefbf7fe8124372e0
Santiago Almendra <santiagoalmendrarojas19@gmail.com>
2021-12-24 17:38:03 -0500

    Se construyeron las Entidades de PersonalCosecha

--- src/main/java/co/com/challengeddd/domain/personalcosecha/CosechadorAgricola.java
+++ src/main/java/co/com/challengeddd/domain/personalcosecha/CosechadorAgricola.java
@@ -1,4 +1,80 @@
-package co.com.challengeDDD.domain.personalcosecha;
+package co.com.challengeddd.domain.personalcosecha;
 
-public class CosechadorAgricola {
+import co.com.challengeddd.domain.general.values.Nombre;
+import co.com.challengeddd.domain.general.values.NumeroCelular;
+import co.com.challengeddd.domain.general.values.Sala;
+import co.com.challengeddd.domain.general.values.TipoBandeja;
+import co.com.challengeddd.domain.personalcosecha.values.IdCosechadorAgricola;
+import co.com.challengeddd.domain.personalcosecha.values.TipoCuchillo;
+import co.com.challengeddd.domain.producciondia.values.TamañoChampiñon;
+import co.com.sofka.domain.generic.Entity;
+
+import java.util.Objects;
+
+public class CosechadorAgricola extends Entity<IdCosechadorAgricola> {
+
+    private Nombre nombre;
+    private NumeroCelular numeroCelular;
+    private TipoCuchillo tipoCuchillo;
+    private Sala sala;
+    private TamañoChampiñon tamañoChampiñon;
+    private TipoBandeja tipoBandeja;
+
+    public CosechadorAgricola(IdCosechadorAgricola idCosechadorAgricola, Nombre nombre, NumeroCelular numeroCelular, TipoCuchillo tipoCuchillo, Sala sala, TamañoChampiñon tamañoChampiñon, TipoBandeja tipoBandeja){
+        super(idCosechadorAgricola);
+        this.nombre = nombre;
+        this.numeroCelular = numeroCelular;
+        this.tipoCuchillo = tipoCuchillo;
+        this.sala = sala;
+        this.tamañoChampiñon = tamañoChampiñon;
+        this.tipoBandeja = tipoBandeja;
+    }
+
+    public void modificarNombre(Nombre nombre){
+        this.nombre = Objects.requireNonNull(nombre);
+    }
+
+    public void modificarNumeroCelular(NumeroCelular numeroCelular){
+        this.numeroCelular = Objects.requireNonNull(numeroCelular);
+    }
+
+    public void modificarTipoCuchillo(TipoCuchillo tipoCuchillo){
+        this.tipoCuchillo = Objects.requireNonNull(tipoCuchillo);
+    }
+
+    public void modificarSala(Sala sala){
+        this.sala = Objects.requireNonNull(sala);
+    }
+
+    public void modificarTamañoChampiñon(TamañoChampiñon tamañoChampiñon){
+        this.tamañoChampiñon = Objects.requireNonNull(tamañoChampiñon);
+    }
+
+    public void modificarTipoBandeja(TipoBandeja tipoBandeja){
+        this.tipoBandeja = Objects.requireNonNull(tipoBandeja);
+    }
+
+    public Nombre nombre() {
+        return nombre;
+    }
+
+    public NumeroCelular numeroCelular() {
+        return numeroCelular;
+    }
+
+    public TipoCuchillo tipoCuchillo() {
+        return tipoCuchillo;
+    }
+
+    public Sala sala() {
+        return sala;
+    }
+
+    public TamañoChampiñon tamañoChampiñon() {
+        return tamañoChampiñon;
+    }
+
+    public TipoBandeja tipoBandeja() {
+        return tipoBandeja;
+    }
 }
--- src/main/java/co/com/challengeddd/domain/personalcosecha/Tajador.java
+++ src/main/java/co/com/challengeddd/domain/personalcosecha/Tajador.java
@@ -1,4 +1,58 @@
-package co.com.challengeDDD.domain.personalcosecha;
+package co.com.challengeddd.domain.personalcosecha;
 
-public class Tajador {
+import co.com.challengeddd.domain.general.values.Nombre;
+import co.com.challengeddd.domain.general.values.NumeroCelular;
+import co.com.challengeddd.domain.personalcosecha.values.IdTajador;
+import co.com.challengeddd.domain.personalcosecha.values.TecnicaTajado;
+import co.com.challengeddd.domain.personalcosecha.values.TipoCuchillo;
+import co.com.sofka.domain.generic.Entity;
+
+import java.util.Objects;
+
+public class Tajador extends Entity<IdTajador> {
+
+    private Nombre nombre;
+    private NumeroCelular numeroCelular;
+    private TipoCuchillo tipoCuchillo;
+    private TecnicaTajado tecnicaTajado;
+
+    public Tajador(IdTajador idTajador, Nombre nombre, NumeroCelular numeroCelular, TipoCuchillo tipoCuchillo, TecnicaTajado tecnicaTajado){
+        super(idTajador);
+        this.nombre = nombre;
+        this.numeroCelular = numeroCelular;
+        this.tipoCuchillo = tipoCuchillo;
+        this.tecnicaTajado = tecnicaTajado;
+    }
+
+    public void modificarNombre(Nombre nombre){
+        this.nombre = Objects.requireNonNull(nombre);
+    }
+
+    public void modificarNumeroCelular(NumeroCelular numeroCelular){
+        this.numeroCelular = Objects.requireNonNull(numeroCelular);
+    }
+
+    public void modificarTipoCuchillo(TipoCuchillo tipoCuchillo){
+        this.tipoCuchillo = Objects.requireNonNull(tipoCuchillo);
+    }
+
+    public void modificarTecnicaTajado(TecnicaTajado tecnicaTajado){
+        this.tecnicaTajado = Objects.requireNonNull(tecnicaTajado);
+    }
+
+    public Nombre nombre() {
+        return nombre;
+    }
+
+    public NumeroCelular numeroCelular() {
+        return numeroCelular;
+    }
+
+    public TipoCuchillo tipoCuchillo() {
+        return tipoCuchillo;
+    }
+
+    public TecnicaTajado tecnicaTajado() {
+        return tecnicaTajado;
+    }
 }
