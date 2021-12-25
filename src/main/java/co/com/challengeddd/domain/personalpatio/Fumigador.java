82743fa3e2120737a81b2a4b1d3d9b895521dbe1
Santiago Almendra <santiagoalmendrarojas19@gmail.com>
2021-12-25 07:40:20 -0500

    Se construyeron las Entity de PersonalPatio

--- src/main/java/co/com/challengeddd/domain/personalpatio/Fumigador.java
+++ src/main/java/co/com/challengeddd/domain/personalpatio/Fumigador.java
@@ -1,4 +1,58 @@
 package co.com.challengeddd.domain.personalpatio;
 
-public class Fumigador {
+import co.com.challengeddd.domain.general.values.Nombre;
+import co.com.challengeddd.domain.general.values.NumeroCelular;
+import co.com.challengeddd.domain.personalpatio.values.AreaAFumigar;
+import co.com.challengeddd.domain.personalpatio.values.IdFumigador;
+import co.com.challengeddd.domain.personalpatio.values.Veneno;
+import co.com.sofka.domain.generic.Entity;
+
+import java.util.Objects;
+
+public class Fumigador extends Entity<IdFumigador> {
+
+    private Nombre nombre;
+    private NumeroCelular numeroCelular;
+    private AreaAFumigar areaAFumigar;
+    private Veneno veneno;
+
+    public Fumigador(IdFumigador idFumigador, Nombre nombre, NumeroCelular numeroCelular, AreaAFumigar areaAFumigar, Veneno veneno){
+        super(idFumigador);
+        this.nombre = nombre;
+        this.numeroCelular = numeroCelular;
+        this.areaAFumigar = areaAFumigar;
+        this.veneno = veneno;
+    }
+
+    public void modificarNombre(Nombre nombre){
+        this.nombre = Objects.requireNonNull(nombre);
+    }
+
+    public void modificarNumeroCelular(NumeroCelular numeroCelular){
+        this.numeroCelular = Objects.requireNonNull(numeroCelular);
+    }
+
+    public void modificarAreaAFumigar(AreaAFumigar areaAFumigar){
+        this.areaAFumigar = Objects.requireNonNull(areaAFumigar);
+    }
+
+    public void modificarVeneno(Veneno veneno){
+        this.veneno = Objects.requireNonNull(veneno);
+    }
+
+    public Nombre nombre() {
+        return nombre;
+    }
+
+    public NumeroCelular numeroCelular() {
+        return numeroCelular;
+    }
+
+    public AreaAFumigar areaAFumigar() {
+        return areaAFumigar;
+    }
+
+    public Veneno veneno() {
+        return veneno;
+    }
 }
--- src/main/java/co/com/challengeddd/domain/personalpatio/Sembrador.java
+++ src/main/java/co/com/challengeddd/domain/personalpatio/Sembrador.java
@@ -1,4 +1,58 @@
 package co.com.challengeddd.domain.personalpatio;
 
-public class Sembrador {
+import co.com.challengeddd.domain.general.values.Nombre;
+import co.com.challengeddd.domain.general.values.NumeroCelular;
+import co.com.challengeddd.domain.general.values.Sala;
+import co.com.challengeddd.domain.personalpatio.values.HerramientaTrabajo;
+import co.com.challengeddd.domain.personalpatio.values.IdSembrador;
+import co.com.sofka.domain.generic.Entity;
+
+import java.util.Objects;
+
+public class Sembrador extends Entity<IdSembrador> {
+
+    private Nombre nombre;
+    private NumeroCelular numeroCelular;
+    private Sala sala;
+    private HerramientaTrabajo herramientaTrabajo;
+
+    public Sembrador(IdSembrador idSembrador, Nombre nombre, NumeroCelular numeroCelular, Sala sala, HerramientaTrabajo herramientaTrabajo){
+        super(idSembrador);
+        this.nombre = nombre;
+        this.numeroCelular = numeroCelular;
+        this.sala = sala;
+        this.herramientaTrabajo = herramientaTrabajo;
+    }
+
+    public void modificarNombre(Nombre nombre){
+        this.nombre = Objects.requireNonNull(nombre);
+    }
+
+    public void modificarNumeroCelular(NumeroCelular numeroCelular){
+        this.numeroCelular = Objects.requireNonNull(numeroCelular);
+    }
+
+    public void modificarSala(Sala sala){
+        this.sala = Objects.requireNonNull(sala);
+    }
+
+    public void modificarHerramientaTrabajo(HerramientaTrabajo herramientaTrabajo){
+        this.herramientaTrabajo = Objects.requireNonNull(herramientaTrabajo);
+    }
+
+    public Nombre nombre() {
+        return nombre;
+    }
+
+    public NumeroCelular numeroCelular() {
+        return numeroCelular;
+    }
+
+    public Sala sala() {
+        return sala;
+    }
+
+    public HerramientaTrabajo herramientaTrabajo() {
+        return herramientaTrabajo;
+    }
 }
