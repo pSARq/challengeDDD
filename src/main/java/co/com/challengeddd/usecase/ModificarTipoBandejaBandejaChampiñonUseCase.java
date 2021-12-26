7b95c3bb0e7aa0f83446f5dcd36731e34d7d9bad
Santiago Almendra <santiagoalmendrarojas19@gmail.com>
2021-12-26 09:25:24 -0500

    Se agregó ModificarTipoBandejaBandejaChampiñonUseCase con Test

---
+++ src/main/java/co/com/challengeddd/usecase/ModificarTipoBandejaBandejaChampiñonUseCase.java
@@ -0,0 +1,17 @@
+package co.com.challengeddd.usecase;
+
+import co.com.challengeddd.domain.producciondia.ProduccionDia;
+import co.com.challengeddd.domain.producciondia.commands.ModificarTipoBandejaBandejaChampiñon;
+import co.com.sofka.business.generic.UseCase;
+import co.com.sofka.business.support.RequestCommand;
+import co.com.sofka.business.support.ResponseEvents;
+
+public class ModificarTipoBandejaBandejaChampiñonUseCase extends UseCase<RequestCommand<ModificarTipoBandejaBandejaChampiñon>, ResponseEvents> {
+    @Override
+    public void executeUseCase(RequestCommand<ModificarTipoBandejaBandejaChampiñon> requestCommand) {
+        ModificarTipoBandejaBandejaChampiñon command = requestCommand.getCommand();
+        ProduccionDia produccionDia = ProduccionDia.from(command.getIdProduccionDia(), retrieveEvents());
+        produccionDia.modificarTipoBandejaBandejaChampiñon(command.getIdBandejaChampiñon(), command.getTipoBandeja());
+        emit().onResponse(new ResponseEvents(produccionDia.getUncommittedChanges()));
+    }
+}
---
+++ src/test/java/co/com/challengeddd/usecase/ModificarTipoBandejaBandejaChampiñonUseCaseTest.java
@@ -0,0 +1,68 @@
+package co.com.challengeddd.usecase;
+
+import co.com.challengeddd.domain.general.values.TamañoChampiñon;
+import co.com.challengeddd.domain.general.values.TipoBandeja;
+import co.com.challengeddd.domain.producciondia.commands.ModificarTipoBandejaBandejaChampiñon;
+import co.com.challengeddd.domain.producciondia.events.AgregadaBandejaChampiñon;
+import co.com.challengeddd.domain.producciondia.events.CreadaProduccionDia;
+import co.com.challengeddd.domain.producciondia.events.ModificadoTipoBandejaBandejaChampiñon;
+import co.com.challengeddd.domain.producciondia.values.IdBandejaChampiñon;
+import co.com.challengeddd.domain.producciondia.values.IdProduccionDia;
+import co.com.challengeddd.domain.producciondia.values.TipoChampiñon;
+import co.com.challengeddd.domain.producciondia.values.TipoEmpaqueBandeja;
+import co.com.sofka.business.generic.UseCaseHandler;
+import co.com.sofka.business.repository.DomainEventRepository;
+import co.com.sofka.business.support.RequestCommand;
+import co.com.sofka.business.support.ResponseEvents;
+import co.com.sofka.domain.generic.DomainEvent;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
+
+@ExtendWith(MockitoExtension.class)
+class ModificarTipoBandejaBandejaChampiñonUseCaseTest {
+
+    @Mock
+    DomainEventRepository repository;
+
+    @Test
+    void modificarTipoBandejaBandejaChampiñon(){
+
+        IdProduccionDia idProduccionDia = IdProduccionDia.of("produccion");
+        IdBandejaChampiñon idBandejaChampiñon = IdBandejaChampiñon.of("bandeja");
+        TipoBandeja tipoBandeja = new TipoBandeja("150");
+        ModificarTipoBandejaBandejaChampiñon command = new ModificarTipoBandejaBandejaChampiñon(idProduccionDia, idBandejaChampiñon, tipoBandeja);
+        ModificarTipoBandejaBandejaChampiñonUseCase useCase = new ModificarTipoBandejaBandejaChampiñonUseCase();
+
+        when(repository.getEventsBy("produccion")).thenReturn(events());
+        useCase.addRepository(repository);
+
+        ResponseEvents events = UseCaseHandler.getInstance()
+                .setIdentifyExecutor(idProduccionDia.value())
+                .syncExecutor(useCase, new RequestCommand<>(command))
+                .orElseThrow();
+        ModificadoTipoBandejaBandejaChampiñon event = (ModificadoTipoBandejaBandejaChampiñon) events.getDomainEvents().get(0);
+
+        Assertions.assertEquals("bandeja", event.getIdBandejaChampiñon().value());
+        Assertions.assertEquals("150", event.getTipoBandeja().value());
+        Mockito.verify(repository).getEventsBy("produccion");
+    }
+
+    private List<DomainEvent> events() {
+        return List.of(new CreadaProduccionDia(
+                new TipoEmpaqueBandeja("Canasta")
+        ), new AgregadaBandejaChampiñon(
+                IdBandejaChampiñon.of("bandeja"),
+                new TipoBandeja("500"),
+                new TipoChampiñon("Entero"),
+                new TamañoChampiñon("Grande")));
+    }
+}
